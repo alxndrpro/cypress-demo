70adf34822d02a85a0b9763561015a207eb11a23
alxndr <alxndr.pro@tutanota.de>
2023-12-23 19:52:54 +0100

    List of Elements

--- cypress/e2e/screenshots.cy.js
+++ cypress/e2e/screenshots.cy.js
@@ -9,6 +9,8 @@ describe('Cypress Screenshots', () => {
   it('Single element screenshot', () => {
       cy.visit('https://example.com/') 
       cy.get('h1').screenshot({overwrite : true})
+
+      cy.get('h1').should('have.length', 1)
     })
 
 })
