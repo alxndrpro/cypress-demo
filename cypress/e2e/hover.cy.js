c1d2faedc9dd6b062dc8422de9bdc00eecc689b4
alxndr <alxndr.pro@tutanota.de>
2023-12-23 19:19:49 +0100

    Device Emulation & Viewports

--- cypress/e2e/hover.cy.js
+++ cypress/e2e/hover.cy.js
@@ -2,6 +2,8 @@
 
 describe('Cypress hover', () => {
   it('Lets test hover workaround', () => {
+    // cy.viewport(800, 600) - option #1
+    cy.viewport('macbook-15')
     cy.visit('https://practice-automation.com/hover/')
     cy.get('#mouse_over').trigger('mouseover')
     cy.get('#mouse_over').should('contain', 'You did it!')
