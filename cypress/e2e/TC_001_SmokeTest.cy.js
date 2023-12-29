1a7fb92c0b1b44722b564ca1149ed8b96d9bed69
alxndr <alxndr.pro@tutanota.de>
2023-12-29 20:34:30 +0100

    Smoke test

---
+++ cypress/e2e/TC_001_SmokeTest.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('TC_001_Smoke_Test', () => {
+
+    it('Should load home page', () => {
+      cy.visit('https://demoblaze.com/index.html')
+        cy.get('.navbar-brand').should('be.visible')
+        cy.get('#navbarExample').should('be.visible')
+
+        cy.get('#cat').should('be.visible')
+        
+        cy.get('#fotcont').should('be.visibleclear')
+    })
+  })
